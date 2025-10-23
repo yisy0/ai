1db5bd5f460374dbc4842b88379d48a4a5fb3bf3
yisy0 <yisy0703@naver.com>
2025-10-23 18:08:30 +0900

    웹표준개론

--- source/02_HTML_CSS/ch01/2.js
+++ source/02_HTML_CSS/ch01/2.js
@@ -1,5 +1,5 @@
-// 2.js:�����κκ�(utf-8)
-name = prompt("�̸���?", "ȫ�浿");  // ��Ҹ� Ŭ���� 'null' ����
+﻿// 2.js:동적인부분(파일->다른이름으로저장->인코딩하여저장->UTF8 선택후 저장)
+name = prompt("이름은?", "홍길동");  // 취소를 클릭시 'null' 리턴
 if (name != 'null') {
-    document.write(name + '�� �ݰ����ϴ�<br>');
+    document.write(name + '님 반갑습니다<br>');
 }
