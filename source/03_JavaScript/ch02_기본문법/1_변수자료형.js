5ec9a264ee262af3f6117f827c5cc5ebec68438d
yisy0 <yisy0703@naver.com>
2025-10-31 17:56:39 +0900

    js 기본문법

--- source/03_JavaScript/ch02_기본문법/1_변수자료형.js
+++ source/03_JavaScript/ch02_기본문법/1_변수자료형.js
@@ -7,3 +7,11 @@ variable = -313131313131.2323;
 console.log('3. varialbe 타입 :', typeof(variable), ' - 값 :', variable);
 variable = false;
 console.log('4. varialbe 타입 :', typeof(variable), ' - 값 :', variable);
+variable = function(){ 
+  alert('함수 속');
+};
+console.log('5. varialbe 타입 :', typeof(variable), ' - 값 :', variable);
+variable = {'name':'홍길동', 'age':20}; // 객체
+console.log('6. varialbe 타입 :', typeof(variable), ' - 값 :', variable.name, variable.age);
+variable = ['홍길동', 10, function(){}, true, {'name':'홍길동'}, [1,2,3]]; // 배열
+console.log('7. varialbe 타입 :', typeof(variable), ' - 값 :', variable.name, variable.age);
