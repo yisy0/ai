2692b64347e442fc4632daf601e5faae564f63e6
yisy0 <yisy0703@naver.com>
2025-10-31 17:40:09 +0900

    JavaScript 기본문법

---
+++ source/03_JavaScript/ch02_기본문법/1_변수자료형.js
@@ -0,0 +1,5 @@
+// 자료형 : string, number, boolean, function, object(array), undefined
+var variable;
+console.log('1. varialbe 타입 :', typeof(variable), ' - 값 :', variable);
+variable = '이름은 \'홍길동\'입니다';
+console.log('2. varialbe 타입 :', typeof(variable), ' - 값 :', variable);
