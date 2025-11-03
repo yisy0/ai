d41b3e1e718c9462e718d677d220ae5ed79a8fd3
yisy0 <yisy0703@naver.com>
2025-11-03 12:18:15 +0900

    js조건문

--- source/03_JavaScript/ch03_조건문/4_삼항연산자.js
+++ source/03_JavaScript/ch03_조건문/4_삼항연산자.js
@@ -2,5 +2,8 @@ let num1 = 20;
 let num2 = 21;
 // let difference; // 차이값 저장할 예정
 if(num1>num2){
-  let difference = 
+  let difference = num1 - num2;
+}else {
+  let difference = num2 - num1;
 }
+console.log('두수의 차이는', difference);
