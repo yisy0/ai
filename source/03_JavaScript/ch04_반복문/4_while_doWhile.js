7931425aebfe7da38d830859daea7eecef73dff9
yisy0 <yisy0703@naver.com>
2025-11-03 16:26:51 +0900

    js반복문

--- source/03_JavaScript/ch04_반복문/4_while_doWhile.js
+++ source/03_JavaScript/ch04_반복문/4_while_doWhile.js
@@ -1,5 +1,10 @@
 // 1초동안 반복하는 while 구문 / do~while 구문
 var cnt = 0; // 반복횟수
-while(1초가 안 지났니?){
-  cnt++; // cnt 1증가
-}
+var startTime = new Date().getTime(); // 시작시점의 밀리세컨
+// while(new Date().getTime() <= startTime + 1000){
+//   cnt++; // cnt 1증가
+// }
+do{
+  cnt++;
+}while(new Date().getTime() <= startTime + 1000);
+console.log('1초동안 while문 수행한 횟수 :',cnt);
