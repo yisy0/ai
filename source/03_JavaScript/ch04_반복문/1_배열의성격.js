60cb7785f85a2efa101d7d676c8c9c6453f280e7
yisy0 <yisy0703@naver.com>
2025-11-03 15:22:21 +0900

    js반복문

--- source/03_JavaScript/ch04_반복문/1_배열의성격.js
+++ source/03_JavaScript/ch04_반복문/1_배열의성격.js
@@ -6,4 +6,9 @@ console.log(2+':'+arr[2]);
 console.log(3+':'+arr[3]);
 console.log(4+':'+arr[4]);
 console.log(5+':'+arr[5]);
-console.log(6+':'+arr[6]);
+console.log(6+':'+arr[6]); // undefined
+arr.push('제일 끝 방 추가');
+console.log(6+':'+arr[6]); 
+var value = arr.pop(); // 제일 끝방의 값을 return하고 없앰
+console.log(6+':'+arr[6]); 
+console.log('삭제된 값:', value);
