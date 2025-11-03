50e2c4d9fb465cff8e37c14da2f01d8416f56859
yisy0 <yisy0703@naver.com>
2025-11-03 15:17:17 +0900

    js반복문

---
+++ source/03_JavaScript/ch04_반복문/1_배열의성격.js
@@ -0,0 +1,9 @@
+var arr = [2, 3, '홍길동', function(){}, [1,2], {'0':273, '1':'hello'}];
+console.log('arr의 타입 :', typeof(arr) + '/값 :', arr);
+console.log(0+':'+arr[0]);
+console.log(1+':'+arr[1]);
+console.log(2+':'+arr[2]);
+console.log(3+':'+arr[3]);
+console.log(4+':'+arr[4]);
+console.log(5+':'+arr[5]);
+console.log(6+':'+arr[6]);
