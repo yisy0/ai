66d0d6c58708903a53f2c0873ccff71a9cb52f60
yisy0 <yisy0703@naver.com>
2025-11-04 10:56:33 +0900

    js함수

--- source/03_JavaScript/ch05_함수/05_매개변수와리턴값.js
+++ source/03_JavaScript/ch05_함수/05_매개변수와리턴값.js
@@ -1,5 +1,8 @@
 console.log(pow(5, 3));
-
+// 선언된 매개변수보다 많은 매개변수로 호출될 경우 : 뒷부분 무시
+console.log(pow(5, 2, 1, 10));
+// 선언된 매개변수보다 적은 매개변수로 호출될 경우 : 전달되지 않은 매개변수는 undefined
+console.log(pow(5));
 function pow(x, y){
   let result = 1;
   for(let cnt=1 ; cnt<=y ; cnt++){
