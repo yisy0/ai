120d93550f13ec2f783b9ce5facc09350829d76c
yisy0 <yisy0703@naver.com>
2025-11-04 11:18:32 +0900

    js함수

--- source/03_JavaScript/ch05_함수/07_array.js
+++ source/03_JavaScript/ch05_함수/07_array.js
@@ -2,6 +2,7 @@
  * 매개변수가 0개 : length가 0인 배열 return
  * 매개변수가 1개 : length가 매개변수만큼의 크기인 배열 return
  * 매개변수가 2개 이상 : 매개변수로 배열을 생성 return */
-function array(){ 
-  
+function array(){ // arguments(매개변수 배열) : 매개변수의 내용
+  console.log(arguments);
 }
+array();
