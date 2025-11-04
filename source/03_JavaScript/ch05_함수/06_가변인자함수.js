b0adfc50a60a28389abad25a823810bc13afc8c5
yisy0 <yisy0703@naver.com>
2025-11-04 11:14:10 +0900

    js함수

--- source/03_JavaScript/ch05_함수/06_가변인자함수.js
+++ source/03_JavaScript/ch05_함수/06_가변인자함수.js
@@ -1,1 +1,14 @@
 // 가변인자함수 : 매개변수 갯수에 따라 변하는 함수. 화살표함수에서는 불가
+// 내장함수 Array()
+var arr1 = [1, 2, '삼', ];
+var arr2 = Array(1, 2, '삼');
+var arr3 = [ , , , ]; // 방의 갯수가 3개인 데이터가 빈 배열
+var arr4 = Array(3);
+var arr5 = []; // 방의 갯수가 0인 배열
+var arr6 = Array();
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
+console.log(arr5);
+console.log(arr6);
