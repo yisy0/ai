c3b39c2cd49045388a644188c5474818626027eb
yisy0 <yisy0703@naver.com>
2025-11-04 11:44:22 +0900

    js함수

--- source/03_JavaScript/ch05_함수/07_array.js
+++ source/03_JavaScript/ch05_함수/07_array.js
@@ -6,6 +6,16 @@ function array(){ // arguments(매개변수 배열) : 매개변수의 내용
   let result = [];
   if(arguments.length==1){
     // result를 argumnets[0] 만큼의 크기인 배열로 : result.push(null)를 arguments[0]번
-    
+    for( let cnt=1 ; cnt<=arguments[0] ; cnt++){
+      result.push(null);
+    }
+  }else if(arguments.length >= 2){
+    // result를 arguments 내용의 배열로 : result.push(arguments[0~끝까지])
+
   }
+  return result;
 }
+var arr1 = array(5);
+var arr2 = array();
+console.log(arr1);
+console.log(arr2);
