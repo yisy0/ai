1b4ae77520933c48ba29be3dd0ba62ae92b88ffa
yisy0 <yisy0703@naver.com>
2025-11-04 12:41:43 +0900

    js함수

---
+++ source/03_JavaScript/ch05_함수/sumAll.js
@@ -0,0 +1,15 @@
+function sumAll(){
+  // 매개변수가 없으면 -999 return / 매개변수 있으면 매개변수 합 return
+  let resultSum = 0;
+  if(arguments.length==0){
+    return -999;
+  }else{
+    for(let data of arguments){
+      resultSum += data;
+    }
+  }
+  return resultSum;
+}
+console.log(sumAll());
+console.log(sumAll(1,3,5,7));
+console.log(sumAll(7));
