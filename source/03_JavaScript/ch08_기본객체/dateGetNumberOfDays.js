a9514270468d21a1f9dd99952c635426bebe72db
yisy0 <yisy0703@naver.com>
2025-11-05 17:39:07 +0900

    js기본객체

--- source/03_JavaScript/ch08_기본객체/dateGetNumberOfDays.js
+++ source/03_JavaScript/ch08_기본객체/dateGetNumberOfDays.js
@@ -5,8 +5,8 @@ Date.prototype.getNumberOfDays = function(that){
   let day = Math.floor(intervalMilisec/(1000*60*60*24)); // 내림
   return day;
 };
-let now = new Date();
-let limitday = new Date(2026, 1, 12, 18, 0, 0); // 26.2.12
-console.log(now.getNumberOfDays(limitday), '일 남음');
-console.log(limitday.getNumberOfDays(now), '일 남음' );
-console.log(now.getNumberOfDays(now));
+// let now = new Date();
+// let limitday = new Date(2026, 1, 12, 18, 0, 0); // 26.2.12
+// console.log(now.getNumberOfDays(limitday), '일 남음');
+// console.log(limitday.getNumberOfDays(now), '일 남음' );
+// console.log(now.getNumberOfDays(now));
