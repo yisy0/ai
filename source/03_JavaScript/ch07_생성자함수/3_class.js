09492bf0520e1f0c7bf9fc36e36ad6af24a3b866
yisy0 <yisy0703@naver.com>
2025-11-05 11:58:13 +0900

    js생성자함수

---
+++ source/03_JavaScript/ch07_생성자함수/3_class.js
@@ -0,0 +1,24 @@
+class Student{
+  constructor(name, kor, mat, eng, sci){
+    this.name = name;
+    this.kor  = kor;
+    this.mat  = mat;
+    this.eng  = eng;
+    this.sci  = sci;
+  }
+  getSum(){
+    return this.kor + this.mat + this.eng + this.sci;
+  }
+  getAvg(){
+    return this.getSum() / 4;
+  }
+  toString(){
+    return 'name : ' + this.name +
+            'kor : ' + this.kor +
+            'mat : ' + this.mat +
+            'eng : ' + this.eng +
+            'sci : ' + this.sci +
+            'sum : ' + this.getSum() +
+            'avg : ' + this.getAvg();
+  }
+}
