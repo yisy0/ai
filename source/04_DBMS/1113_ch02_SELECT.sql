9bcf68aca819519168bf441547393ea314f12cd4
yisy0 <yisy0703@naver.com>
2025-11-13 15:26:51 +0900

    Oracle 검색

--- source/04_DBMS/1113_ch02_SELECT.sql
+++ source/04_DBMS/1113_ch02_SELECT.sql
@@ -11,10 +11,14 @@ DESC EMP;
     -- EMP테이블의 구조
 SELECT EMPNO, ENAME, SAL, JOB FROM EMP; -- EMP테이블의 SELECT절에 지정된 열만 출력
 SELECT EMPNO AS "사 번", ENAME AS "이 름", SAL AS "급여", JOB AS "직책"
-    FROM EMP;  -- 열이름에 별칭을 두는 경우 EX. 열이름 AS 별칭
+    FROM EMP;  -- 열이름에 별칭을 두는 경우 EX. 열이름 AS "별칭"
+SELECT EMPNO "사 번", ENAME "이 름", SAL "급여", JOB
+    FROM EMP;  -- 열이름에 별칭을 두는 경우 EX. 열이름 "별칭", 열이름
+SELECT EMPNO "사 번", ENAME 이름, SAL 급여, JOB
+    FROM EMP;  -- 별칭에 SPACE가 없는 경우는 따옴표 생략가능
 
-
-
+-- 3. 특정 행 출력 : WHERE절(조건절) -- 비교연산자 : 같다(=), 다르다(!=, ^=, <>), >,..
+SELECT EMPNO 사번, ENAME 이름, SAL 급여 FROM EMP WHERE SAL=3000;
 
 
 
