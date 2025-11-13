b5ca43665eade4dc0196ca498c2065459ab5cb28
yisy0 <yisy0703@naver.com>
2025-11-13 17:45:39 +0900

    Oracle 검색

--- source/04_DBMS/1113_ch02_SELECT.sql
+++ source/04_DBMS/1113_ch02_SELECT.sql
@@ -74,30 +74,44 @@ SELECT DISTINCT DEPTNO FROM EMP;
 -- 연습문제 꼭 풀기
 --1. emp 테이블의 구조 출력
 DESC EMP;
+
 --2. emp 테이블의 모든 내용을 출력 
+SELECT * FROM EMP;
 
 --3. 현 scott 계정에서 사용가능한 테이블 출력
+SELECT * FROM TAB;
 
 --4. emp 테이블에서 사번, 이름, 급여, 업무, 입사일 출력
+SELECT EMPNO, ENAME, SAL, JOB, HIREDATE FROM EMP;
 
 --5. emp 테이블에서 급여가 2000미만인 사람의 사번, 이름, 급여 출력
+SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL<2000;
 
 --6. 입사일이 81/02이후에 입사한 사람의 사번, 이름, 업무, 입사일 출력
-
+SELECT EMPNO, ENAME, JOB, HIREDATE FROM EMP 
+    WHERE TO_CHAR(HIREDATE, 'RR/MM') >= '81/02';
+    
 --7. 업무가 SALESMAN인 사람들 모든 자료 출력
+SELECT * FROM EMP WHERE JOB = 'SALESMAN';
 
 --8. 업무가 CLERK이 아닌 사람들 모든 자료 출력
+SELECT * FROM EMP WHERE JOB != 'CLERK';
 
 --9. 급여가 1500이상이고 3000이하인 사람의 사번, 이름, 급여 출력
+SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL>=1500 AND SAL<=3000;
 
 --10. 부서코드가 10번이거나 30인 사람의 사번, 이름, 업무, 부서코드 출력
+SELECT EMPNO, ENAME, JOB, DEPTNO FROM EMP WHERE DEPTNO=10 OR DEPTNO=30;
 
 --11. 업무가 SALESMAN이거나 급여가 3000이상인 사람의 사번, 이름, 업무, 부서코드 출력
+SELECT EMPNO, ENAME, JOB, DEPTNO FROM EMP WHERE JOB='SALESMAN' OR SAL>=3000;
 
 --12. 급여가 2500이상이고 업무가 MANAGER인 사람의 사번, 이름, 업무, 급여 출력
+SELECT EMPNO, ENAME, JOB, SAL FROM EMP WHERE SAL>=2500 AND JOB='MANAGER';
 
 --13.“ename은 XXX 업무이고 연봉은 XX다” 스타일로 모두 출력(연봉은 SAL*12+COMM)
-
+SELECT ENAME || '은 ' || JOB || '업무이고 연봉은 '|| (SAL*12+NVL(COMM, 0)) || '다' 설명
+    FROM EMP;
 
 
 
