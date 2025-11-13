bffc28a05463415e6a674d58b37c89708fd60814
yisy0 <yisy0703@naver.com>
2025-11-13 14:52:26 +0900

    Oracle 검색

--- source/04_DBMS/1113_ch02_SELECT.sql
+++ source/04_DBMS/1113_ch02_SELECT.sql
@@ -1,8 +1,9 @@
 -- [II] SELECT문 - 조회
 
 -- 1. SELECT 문장 작성법
-SELECT * 
-
+SELECT * FROM TAB; -- 현 계정이 가지고 있는 테이블 정보(실행:CTRL+ENTER)
+SELECT * FROM DEPT; -- DEPT 테이블의 모든 열, 모든 행
+SELECT * FROM SALGRADE; -- 
 
 
 
