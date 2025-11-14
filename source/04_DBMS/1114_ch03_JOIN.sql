97fe95244744a12324d9ac3e7b5d3e19b0f670c1
yisy0 <yisy0703@naver.com>
2025-11-14 14:28:21 +0900

    Oracle sql연산자

--- source/04_DBMS/1114_ch03_JOIN.sql
+++ source/04_DBMS/1114_ch03_JOIN.sql
@@ -78,7 +78,9 @@ SELECT * FROM EMP, SALGRADE WHERE ENAME='SCOTT' AND SAL BETWEEN LOSAL AND HISAL;
         ORDER BY DNAME, 연봉;
     -- 탄탄4. 이름, 업무, 급여, 등급, 부서코드, 부서명 출력. 급여가 1000~3000사이. 
         -- 정렬조건 : 부서별, 부서같으면 업무별, 업무같으면 급여 큰순
-    -- EQUI + NonEQUI
+    SELECT ENAME, JOB, SAL, GRADE, DEPTNO, DNAME
+        FROM EMP E, DEPT D, SALGRADE
+        WHERE E.DEPTNO=D.DEPTNO AND SAL BETWEEN LOSAL AND HISAL
     -- 탄탄5. 이름, 급여, 등급, 입사일, 근무지. 81년에 입사한 사람. 등급 큰순
 
 
