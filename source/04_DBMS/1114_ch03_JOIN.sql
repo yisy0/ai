b497e7f9cfd76899a8df60c7621959ddd4fb2a7d
yisy0 <yisy0703@naver.com>
2025-11-14 14:23:26 +0900

    Oracle sql연산자

--- source/04_DBMS/1114_ch03_JOIN.sql
+++ source/04_DBMS/1114_ch03_JOIN.sql
@@ -64,10 +64,12 @@ SELECT * FROM EMP, SALGRADE WHERE ENAME='SCOTT' AND SAL BETWEEN LOSAL AND HISAL;
     -- 탄탄1. Comm이 null이 아닌 사원의 이름, 급여, 등급, 부서번호, 부서이름, 근무지를 출력.
     SELECT ENAME, SAL, GRADE, E.DEPTNO, DNAME, LOC
         FROM EMP E, DEPT D, SALGRADE
-        WHERE E.DEPTNO=D.DEPTNO AND SAL BETWEEN LOSAL AND HISAL 
+        WHERE E.DEPTNO=D.DEPTNO AND SAL BETWEEN LOSAL AND HISAL ;
             AND COMM IS NOT NULL;
     -- 탄탄2. 이름, 급여, 입사일, 급여등급
-    -- NonEQUI
+    SELECT ENAME, SAL, HIREDATE, GRADE
+        FROM EMP, SALGRADE
+        WHERE SAL BETWEEN LOSAL AND HISAL;
     -- 탄탄3. 이름, 급여, 급여등급, 연봉, 부서명을 부서명순으로 정렬하여 출력.
         -- 부서가 같으면 연봉순. 연봉=(sal+comm)*12 comm이 null이면 0
     -- EQUI + NonEQUI
