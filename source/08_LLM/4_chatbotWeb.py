60ac701a81882bb782cc53667bd5e88d4bee6c98
yisy0 <yisy0703@naver.com>
2025-12-11 12:08:55 +0900

    openAI API

--- source/08_LLM/4_chatbotWeb.py
+++ source/08_LLM/4_chatbotWeb.py
@@ -11,11 +11,26 @@ def main():
   if 'messages' not in st.session_state:
     st.session_state.messages = [
       {'role':"system", "content":"당신은 유능한 AI 상담원입니다"},
-      {'role':"user", "content":"2002년 월드컵 성적은?"},
-      {'role':"assistant", "content":"4강"},
     ]
   # 대화 이력 표시(system 메세지는 제외)
   for msg in st.session_state.messages[1:]:
     st.chat_message(msg["role"]).write(msg["content"])
+
+  # 사용자 질문 입력받아 질문하기
+  if prompt := st.chat_input("메세지를 입력하세요"):
+    prompt = prompt.strip()
+    # 사용자 질문을 session 추가하고 출력
+    st.session_state.messages.append({"role":"user", "content":prompt})
+    st.chat_message("user").write(prompt)
+    # AI 응답을 받아 session 추가하고 출력
+    client = OpenAI()
+    response = client.chat.completions.create(
+      model = "gpt-4.1-nano",
+      messages= st.session_state.messages
+    )
+    reply = response.choices[0].message.content
+    st.session_state.messages.append({"role":"assistant", "content":reply})
+    st.chat_message("assistant").write(reply)
+
 if __name__=="__main__":
   main()
