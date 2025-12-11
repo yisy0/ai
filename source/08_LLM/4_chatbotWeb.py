22687b610287010291e71e51469ef5828736dff1
yisy0 <yisy0703@naver.com>
2025-12-11 11:44:31 +0900

    openAI API

--- source/08_LLM/4_chatbotWeb.py
+++ source/08_LLM/4_chatbotWeb.py
@@ -1,3 +1,11 @@
 import streamlit as st
 from openai import OpenAI
 from dotenv import load_dotenv
+import os
+def main():
+  load_dotenv()
+  st.set_page_config(page_title="비추 쳇봇")
+  st.text(os.getenv('OPENAI_API_KEY'))
+
+if __name__=="__main__":
+  main()
