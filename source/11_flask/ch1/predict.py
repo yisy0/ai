bcc566d6bf43accc8118581048eb26e732556c4d
yisy0 <yisy0703@naver.com>
2025-12-22 10:26:54 +0900

    flask 모델예측

--- source/11_flask/ch1/predict.py
+++ source/11_flask/ch1/predict.py
@@ -1,4 +1,4 @@
 # predict.py 파일
 # 가상환경 만들기 : python -m venv .venv : .venv가상환경 생성
 # 가상환경 들어가기 : .venv\Scripts\activate
-# python -m pip install --upgrade pip
+# pip업그레이드 : python -m pip install --upgrade pip
