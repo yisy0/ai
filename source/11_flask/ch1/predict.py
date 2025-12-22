355eecf209f8f083ac000eca27f5e784e39e4d97
yisy0 <yisy0703@naver.com>
2025-12-22 11:25:44 +0900

    flask 모델예측

--- source/11_flask/ch1/predict.py
+++ source/11_flask/ch1/predict.py
@@ -12,3 +12,17 @@
 # pip freeze > requirements.txt
 # requirements.txt 팀원끼리 공유
 # requirements.txt있는 라이브러리를 똑같이 설치 : pip install -r requirements.txt
+
+import joblib
+loaded_model = joblib.load('model/apt.joblib')
+
+def predict_apt_price(year, square, floor):  
+  input_data = [[int(year), int(square), int(floor), 1]]
+  result = round(loaded_model.predict(input_data)[0] * 10000)
+  return format(result, ',') + '원입니다'
+
+if __name__=="__main__":
+  year = input('몇년?')
+  square = input('몇 제곱미터?')
+  floor = input('몇층?')
+  print('예측한 금액은 ', predict_apt_price(year, square, floor))
