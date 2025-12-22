fbb90eab7f2052e15d00dfa18d4eda171cfe254d
yisy0 <yisy0703@naver.com>
2025-12-22 10:48:55 +0900

    flask 모델예측

--- source/11_flask/ch1/predict.py
+++ source/11_flask/ch1/predict.py
@@ -2,10 +2,13 @@
 # 가상환경 만들기 : python -m venv .venv : .venv가상환경 생성
 # 가상환경 들어가기 : .venv\Scripts\activate
 # pip업그레이드 : python -m pip install --upgrade pip
+
 # pip install statsmodels joblib
 # pip install xlwings
 # pip install flask
 
-# .ignore파일에 .venv/와  .idea/추가
+# .gitignore파일에 .venv/와  .idea/추가
 
 # pip freeze > requirements.txt
+# requirements.txt 팀원끼리 공유
+# requirements.txt있는 라이브러리를 똑같이 설치 : pip install -r requirements.txt
