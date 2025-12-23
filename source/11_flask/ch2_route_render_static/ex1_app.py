7b3ec9851b380f5d6ce58b3facebd1edc857fe09
yisy0 <yisy0703@naver.com>
2025-12-23 10:17:12 +0900

    flask라우팅 렌더링

--- source/11_flask/ch2_route_render_static/ex1_app.py
+++ source/11_flask/ch2_route_render_static/ex1_app.py
@@ -14,3 +14,15 @@ app = Flask(__name__) # 웹서버 객체(앱 인스턴스 생성)
 @app.route("/") # 데코레이터를 통해 가능한 url 등록
 def main_handler():
   return "<H1>Hello, World</h1>"
+
+@app.route("/apt")
+def apt_handler():
+  return "<h1>예상 금액은 1,000원입니다</h1>"
+  # return {
+  #   'price':'1,000',
+  #   'unit':'won'
+  # }
+# app.py에서 실행 : flask run --port=80 --debug
+# app.py가 아닌 파일 플라스크 실행 : python ex1_app.py
+if __name__=="__main__":
+  app.run(port=80, debug=True) # 소스 수정시 서버 자동 재시작, port=80번
