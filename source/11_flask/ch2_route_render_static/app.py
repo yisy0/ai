e19dc379762506ba019df48a62ddc24bb9fb8c48
yisy0 <yisy0703@naver.com>
2025-12-23 09:51:34 +0900

    flask라우팅 렌더링

--- source/11_flask/ch2_route_render_static/app.py
+++ source/11_flask/ch2_route_render_static/app.py
@@ -1,3 +1,7 @@
 # PowerShell 관리자 모드 : Get-ExecutionPolicy실행 => Restricted
+#                         Set-ExecutionPolicy RemoteSigned 
 # app.py 생성 후 ctrl+j 터미널 창을 열기
-# 가상환경 만들기 : python -m 
+# 가상환경 만들기 : python -m venv .venv
+# 가상환경 들어가기 : .venv\Scripts\activate
+# python -m pip install --upgrade pip
+# pip install flask
