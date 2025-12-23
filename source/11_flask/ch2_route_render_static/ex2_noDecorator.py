e968264e50d5b4acd634d42387818845fa0541c1
yisy0 <yisy0703@naver.com>
2025-12-23 10:24:00 +0900

    flask라우팅 렌더링

--- source/11_flask/ch2_route_render_static/ex2_noDecorator.py
+++ source/11_flask/ch2_route_render_static/ex2_noDecorator.py
@@ -1,13 +1,18 @@
 # 데코레이터(@) : 대상 함수를 감싸 함수의 앞뒤에 부가적인 구문을 추가 반복 작업
+def check(func):
+  def wrapper():
+    print(func.__name__, '함수 전처리')
+    func()
+    print(func.__name__, '함수 후처리')
+  return wrapper
+
 def hello():
-  print(hello.__name__, '함수 전처리')
   print('Hello')
-  print(hello.__name__, '함수 후처리')
 def world():
-  print(world.__name__, '함수 전처리')
   print('world')
-  print(world.__name__, '함수 후처리')
 
 if __name__=="__main__":
-  hello()
-  world()
+  wrapper_hello = check(hello)
+  wrapper_hello()
+  wrapper_world = check(world)
+  wrapper_world()
