cc00f951ecf2e6cdc0700c472543ca9192d80bae
yisy0 <yisy0703@naver.com>
2025-12-23 09:56:59 +0900

    flask라우팅 렌더링

--- source/11_flask/ch2_route_render_static/app.py
+++ source/11_flask/ch2_route_render_static/app.py
@@ -3,5 +3,8 @@
 # app.py 생성 후 ctrl+j 터미널 창을 열기
 # 가상환경 만들기 : python -m venv .venv
 # 가상환경 들어가기 : .venv\Scripts\activate
-# python -m pip install --upgrade pip
+# pip업그레이드 : python -m pip install --upgrade pip
 # pip install flask
+
+# pip freeze > requirements.txt
+# pip install -r requirements.txt(내일)
