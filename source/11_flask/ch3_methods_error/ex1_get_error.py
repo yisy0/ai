6c779adbe65081fae4d3079025eb62812dbbdd31
yisy0 <yisy0703@naver.com>
2025-12-24 09:45:08 +0900

    flask get/예외처리페이지

--- source/11_flask/ch3_methods_error/ex1_get_error.py
+++ source/11_flask/ch3_methods_error/ex1_get_error.py
@@ -1,3 +1,3 @@
 # 가상환경 생성방법1 : python -m venv .venv
 # 가상환경 생성방법2 : ctrl+shift+p => select interpreter => 가상환경만들기 
-#                  => .venv로 가상환경만들기 => 인터프리터경로입력
+#        => .venv로 가상환경만들기 => 인터프리터경로입력 => 찾기(python.exe)
