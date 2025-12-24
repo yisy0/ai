20ad8b02e8e596ae907f70b12d94f06719126dc6
yisy0 <yisy0703@naver.com>
2025-12-24 10:00:28 +0900

    flask get/예외처리페이지

--- source/11_flask/ch3_methods_error/ex1_get_error.py
+++ source/11_flask/ch3_methods_error/ex1_get_error.py
@@ -1,3 +1,6 @@
 # 가상환경 생성방법1 : python -m venv .venv
 # 가상환경 생성방법2 : ctrl+shift+p => select interpreter => 가상환경만들기 
 #        => .venv로 가상환경만들기 => 인터프리터경로입력 => 찾기(python.exe)
+# 가상환경 생성방법2를 사용하면 pip 업그레이드와 가상환경 들어가기까지 포함
+# pip install pydantic
+# pip freeze > requirements.txt
