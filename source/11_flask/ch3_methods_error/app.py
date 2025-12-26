78b0ba7a893cb0f80bf015e009c1b6e2ab403824
yisy0 <yisy0703@naver.com>
2025-12-26 09:55:59 +0900

    CRUD method방식

--- source/11_flask/ch3_methods_error/app.py
+++ source/11_flask/ch3_methods_error/app.py
@@ -5,4 +5,11 @@ from filters import mask_password
 
 app = Flask(__name__)
 app.template_filter("mask_pw")(mask_password) # 필터 추가
-@app.errorhandler(404)
+@app.errorhandler(404) # 예외 페이지 처리
+def errorhandler(error):
+  print('★', error)
+  return render_template("error_page.html")
+
+@app.route("/", methods=["GET"])
+def index():
+  return render_template("2_crud/index.html")
