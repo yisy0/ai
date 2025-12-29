b6fc9d3df712fca942c4904a67bf55c3d0b1e2f3
yisy0 <yisy0703@naver.com>
2025-12-29 09:56:29 +0900

    Jinja2 문법

--- source/11_flask/ch4_Jinja2/ex1_app.py
+++ source/11_flask/ch4_Jinja2/ex1_app.py
@@ -3,5 +3,5 @@
   # 기본 제공 필터 : lower, upper, length, capitalize, title, trim, replace
   # 형변환 제공 필터 : int, float, string
 # 2. 제어문 {% %}
-## 2-1 조건문
+## 2-1 조건문 {% if 조건 %}A태그{%elif 조건 %}
 ## 2-2 반복문
