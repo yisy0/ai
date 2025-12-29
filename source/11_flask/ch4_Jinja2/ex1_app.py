958160066af45190e6de34e4d61888df2c2f130c
yisy0 <yisy0703@naver.com>
2025-12-29 09:55:57 +0900

    Jinja2 문법

--- source/11_flask/ch4_Jinja2/ex1_app.py
+++ source/11_flask/ch4_Jinja2/ex1_app.py
@@ -1,3 +1,7 @@
 ### Jinja2 Templates 문법 ###
 # 1. 변수 : {{var}} 또는 {{var | filter}} 사용
-  # 기본 제공 필터 :
+  # 기본 제공 필터 : lower, upper, length, capitalize, title, trim, replace
+  # 형변환 제공 필터 : int, float, string
+# 2. 제어문 {% %}
+## 2-1 조건문
+## 2-2 반복문
