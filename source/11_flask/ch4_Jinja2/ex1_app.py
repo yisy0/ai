378a5934fccc14abc0fdda7e21def9c2687bc425
yisy0 <yisy0703@naver.com>
2025-12-29 10:04:45 +0900

    Jinja2 문법

--- source/11_flask/ch4_Jinja2/ex1_app.py
+++ source/11_flask/ch4_Jinja2/ex1_app.py
@@ -3,5 +3,12 @@
   # 기본 제공 필터 : lower, upper, length, capitalize, title, trim, replace
   # 형변환 제공 필터 : int, float, string
 # 2. 제어문 {% %}
-## 2-1 조건문 {% if 조건 %}A태그{%elif 조건 %}
+## 2-1 조건문 {% if 조건1 %}A태그{% elif 조건2 %}B태그{% else %}C태그{% endif %}
 ## 2-2 반복문
+##    {% for var in vars %}
+##      <태그>{{loop.index}}. {{var}}</태그>
+##        loop.index : 1부터 순번 / loop.first:첫번째 loop인지 여부 / loop.last:마지막 loop인지 여부
+##    {% endfor %}
+# 3. 해더나 풋터 {% include "header.html" %} {% extends "base.html" %}
+# 4. 서브 블럭 {% block %} {% endblock %}
+# 5. 주석 {# 주석 #}
