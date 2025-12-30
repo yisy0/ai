69979996c5a638226a0924cfbbe6042b140641b0
yisy0 <yisy0703@naver.com>
2025-12-30 12:36:59 +0900

    할일CRUD웹

--- source/04_DBMS/1118_MySQL.sql
+++ source/04_DBMS/1118_MySQL.sql
@@ -214,12 +214,3 @@ select sysdate(); -- mysql는 select절 가능
 	-- %Y(년도 4자리) %y(년도 2자리), %m(월) %d(일 01, 02,.), %c(일1,2)
     -- %h(12시) %H(24시) %i(분) %s(초) %p(오전)
 select date_format(sysdate(), '%y년 %m월 %d일 %p %h:%i:%s') 지금;
-
-# 프로젝트 기간 DB 서버 만들때 필요
-CREATE USER 'remoteuser'@'%' IDENTIFIED BY 'mbcacademy0212!';
-GRANT ALL PRIVILEGES ON *.* TO 'remoteuser'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-show databases;
-use devdb;
-select database();
-show tables;
