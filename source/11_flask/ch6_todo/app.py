f633e2d805a7b3fbea55c7e5371910c29578a101
yisy0 <yisy0703@naver.com>
2025-12-30 17:49:16 +0900

    할일CRUD웹

--- source/11_flask/ch6_todo/app.py
+++ source/11_flask/ch6_todo/app.py
@@ -1,5 +1,6 @@
 # 가상환경 만들기 : python -m venv .venv
 # 가상환경 들어가기 : .venv\Scripts\activate
-# python.exe -m pip install --upgrade pip
+# python -m pip install --upgrade pip
 # pip install flask pydantic cx_Oracle
 # pip freeze > requirements.txt
+# pip install -r requirements.txt
