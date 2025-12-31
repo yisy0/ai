a94ca3a0478c2709e9aced3806ccc39dc3497046
yisy0 <yisy0703@naver.com>
2025-12-31 09:42:21 +0900

    할일CRUD웹

---
+++ lecNote/11_flask/ch6/database/connection.py
@@ -0,0 +1,11 @@
+import cx_Oracle
+import os
+from dotenv import load_dotenv
+load_dotenv()
+dbserver_ip = os.getenv('DBSERVER_IP')
+oracle_port = os.getenv('ORACLE_PORT')
+oracle_user = os.getenv('ORACLE_USER')
+oracle_password = os.getenv('ORACLE_PASSWORD')
+conn = cx_Oracle.connect(oracle_user, 
+                  oracle_password,
+                  f"{dbserver_ip}:{oracle_port}/xe")
