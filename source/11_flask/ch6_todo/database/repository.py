845efc4244365537db4deb6d35712395e50c76bd
yisy0 <yisy0703@naver.com>
2025-12-31 10:24:03 +0900

    할일CRUD웹

--- source/11_flask/ch6_todo/database/repository.py
+++ source/11_flask/ch6_todo/database/repository.py
@@ -15,3 +15,43 @@ def get_todos(order:str="asc") -> List[dict]:
   todos = [dict(zip(keys, row)) for row in result]
   cursor.close()
   return todos
+
+def get_next_id() -> int:
+  cursor = conn.cursor()
+  sql = "SELECT NVL(MAX(ID), 0)+1 FROM TODO"
+  cursor.execute(sql)
+  result = cursor.fetchone() # 반환값은 (4,) 형태의 tuple
+  cursor.close()
+  return result[0]
+
+def get_todo(id:int) -> Todo:
+  cursor = conn.cursor()
+  sql = "SELECT * FROM TODO WHERE ID = :id"
+  cursor.execute(sql, {"id":id})
+  result = cursor.fetchone() # 반환값은 tuple
+  keys = [desc[0].lower() for desc in cursor.description] # ['id', 'content', 'is_done']
+  todo = dict(zip(keys, result))
+  cursor.close()
+  return todo
+
+def create_todo(todo:Todo) -> int:
+  cursor = conn.cursor()
+  sql = "INSERT INTO TODO (ID, CONTENT) VALUES (TODO_SQ.NEXTVAL, :content)"
+  cursor.execute(sql, {'content':todo.content})
+  rows = cursor.rowcount # insert 한 행수
+  cursor.close()
+  return rows # insert 성공시 1 반환
+
+def update_todo(todo:Todo) -> int:
+  cursor = conn.cursor()
+  sql = "UPDATE TODO SET CONTENT = :content, IS_DONE = :is_done WHERE ID = :id"
+  cursor.execute(sql, {'content':todo.content, 'is_done':todo.is_done, 'id':todo.id})
+  rows = cursor.rowcount # update 한 행수
+  cursor.close()
+  return rows # update 성공시 1 반환
+
+if __name__ == "__main__":
+  print('전체 목록 ',get_todos())
+  print('next id ',get_next_id())
+
+# 실행방법 python -m database.repository
