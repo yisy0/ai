d7245af67c3bf973cafa6560b193cd7dda3e750d
yisy0 <yisy0703@naver.com>
2025-12-31 10:43:37 +0900

    할일CRUD웹

--- source/11_flask/ch6_todo/app.py
+++ source/11_flask/ch6_todo/app.py
@@ -1,6 +1,19 @@
 # 가상환경 만들기 : python -m venv .venv
 # 가상환경 들어가기 : .venv\Scripts\activate
 # python -m pip install --upgrade pip
-# pip install flask pydantic cx_Oracle
+# pip install flask pydantic cx_Oracle python-dotenv
 # pip freeze > requirements.txt
 # pip install -r requirements.txt
+
+from flask import Flask, request, render_template
+from database.repository import get_todos, get_next_id, get_todo
+from database.repository import create_todo, update_todo, delete_todo
+from flask import redirect, url_for, abort
+from flask import session #  로그인/로그아웃
+from models import Todo
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
