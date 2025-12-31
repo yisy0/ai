31da2396a761b11ac265f64aa86964fa9f88179e
yisy0 <yisy0703@naver.com>
2025-12-31 10:12:47 +0900

    할일CRUD웹

--- source/11_flask/ch6_todo/database/repository.py
+++ source/11_flask/ch6_todo/database/repository.py
@@ -3,3 +3,14 @@ from models import Todo
 from typing import List # 타입 체크용
 # get_todos의 매개변수는 문자로 order를 전달받아 return dict list를 반환
 def get_todos(order:str="asc") -> List[dict]:
+  cursor = conn.cursor()
+  if order == "asc":
+    sql = "SELECT * FROM TODO ORDER BY ID"
+  else:
+    sql = "SELECT * FROM TODO ORDER BY ID DESC"
+  cursor.execute(sql)
+  result = cursor.fetchall()
+  # ["ID", "CONTENT", "IS_DONE"]
+  keys = [desc[0].lower() for desc in cursor.description] 
+  
+  return result
