33c1af6e1397670d2247ee3b93e4bb948a00190f
yisy0 <yisy0703@naver.com>
2026-01-14 09:44:53 +0900

    Django구조

--- source/12_django/ch01_hello/home/views.py
+++ source/12_django/ch01_hello/home/views.py
@@ -1,5 +1,7 @@
+# home/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {"msg":"Django(장고)"})
