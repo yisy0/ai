6727c078ff59c3d1e308dcba8ef246ef5d07c9b8
yisy0 <yisy0703@naver.com>
2026-01-15 11:06:08 +0900

    Django구조

---
+++ source/12_django/ch02_wordcnt/wordcnt/urls.py
@@ -0,0 +1,12 @@
+# wordcnt패키지 안의 urls 모듈
+# wordcnt/         : name=wordcnt:wordinput
+# wordcnt/about/   : name=wordcnt:abount
+# wordcnt/result/  : name=wordcnt:result
+from django.urls import path
+from wordcnt.views import wordinput
+app_name = "wordcnt"
+urlpatterns = [
+  path("", wordinput, name="wordinput"), # /wordcnt/ 단어입력 받는 페이지
+  # path("about/", about, name="about"), # /wordcnt/about/ 도움말
+  # path("result/", result, name="result"), # /wordcnt/result/ 단어입력결과
+]
