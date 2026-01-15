c4cc489882cd789172ecd677316e1cf30a1cc90e
yisy0 <yisy0703@naver.com>
2026-01-15 09:50:00 +0900

    Django구조

--- source/12_django/ch02_wordcnt/home/views.py
+++ source/12_django/ch02_wordcnt/home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpRequest
 # Create your views here.
-def index(request):
+def index(request:HttpRequest) -> HttpResponse:
   context = {"msg":"WordCount Welcome Page",
             "greeting":"Hello, Django(장고)"}
   return render(request=request,
                 template_name="home/index.html",
                 context=context)
+def test(request:HttpRequest) -> HttpResponse:
+  return HttpResponse("""<h1>TEST PAGE</h1>
+                      
+                      """)
