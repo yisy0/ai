5135f19c591745590a75862e525f0cb53661b30b
yisy0 <yisy0703@naver.com>
2026-01-15 10:53:37 +0900

    Django구조

--- source/12_django/ch02_wordcnt/ch02/urls.py
+++ source/12_django/ch02_wordcnt/ch02/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home import views
 
 urlpatterns = [
@@ -24,4 +24,5 @@ urlpatterns = [
     path("test/", views.test, name="test"),
     path("showId/<int:id>/", views.showIntId, name="showIntId"),
     path("showId/<str:id>/", views.showStrId, name="showStrId"),
+    path("wordcnt/", include("wordcnt.urls")),
 ]
