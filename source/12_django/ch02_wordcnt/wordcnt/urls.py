5fee0f6f4b5a417c5465444aedb32629bc58af9a
yisy0 <yisy0703@naver.com>
2026-01-16 10:07:06 +0900

    Django구조

--- source/12_django/ch02_wordcnt/wordcnt/urls.py
+++ source/12_django/ch02_wordcnt/wordcnt/urls.py
@@ -3,10 +3,10 @@
 # wordcnt/about/   : name=wordcnt:abount
 # wordcnt/result/  : name=wordcnt:result
 from django.urls import path
-from wordcnt.views import wordinput, about
+from wordcnt.views import wordinput, about, result
 app_name = "wordcnt"
 urlpatterns = [
   path("", wordinput, name="wordinput"), # /wordcnt/ 단어입력 받는 페이지
   path("about/", about, name="about"), # /wordcnt/about/ 도움말
-  # path("result/", result, name="result"), # /wordcnt/result/ 단어입력결과
+  path("result/", result, name="result"), # /wordcnt/result/ 단어입력결과
 ]
--- source/12_django/ch02_wordcnt/wordcnt/views.py
+++ source/12_django/ch02_wordcnt/wordcnt/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def wordinput(request):
   return render(request, "wordcnt/wordinput.html")
 
 def about(request):
   return render(request, "wordcnt/about.html")
+
+def result(request):
+  print(request.GET)
+  return HttpResponse(request.GET.items())
