e7482f827e6253bd077b6e7851ba9e16ab2ee7b5
yisy0 <yisy0703@naver.com>
2026-01-16 10:16:52 +0900

    Django구조

--- source/12_django/ch02_wordcnt/wordcnt/views.py
+++ source/12_django/ch02_wordcnt/wordcnt/views.py
@@ -8,5 +8,19 @@ def about(request):
   return render(request, "wordcnt/about.html")
 
 def result(request):
-  print(request.GET)
-  return HttpResponse(request.GET.items())
+  #print(request.GET)
+  # fulltxt = request.GET['fulltxt']
+  fulltxt = request.GET.get('fulltxt', '') # 홍길동 홍길동 아자
+  strlength = len(fulltxt) # 글자수(10)
+  words = fulltxt.split() # space단위로 단어 분리 ['홍길동', '홍길동', '아자']
+  words_dic = dict() # 빈딕셔너리 => {'홍길동':2, '아자':1}
+  for word in words:
+    if word in words_dic.keys():
+      words_dic[word] += 1 # {'홍길동':2}
+    else:
+      words_dic[word] = 1 # {'홍길동':1, '아자':1 }
+  #  {'홍길동':2, '아자':1}
+  context = {
+    
+  }
+  return HttpResponse()
