b824e17f9de31324c7340b963d29059db193cad1
yisy0 <yisy0703@naver.com>
2026-01-20 10:01:57 +0900

    Django ORM실습

--- source/12_django/ch03_orm/student/views.py
+++ source/12_django/ch03_orm/student/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from student.models import Student
 # Create your views here.
+def list(request):
+  students = Student.objects.all() # select * from student_student
+  #return HttpResponse(students)
+  return render(request, "student/list.html", {"students":students})
