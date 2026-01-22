0f2db4784c6d137b463185d8f931dd67fde5bfff
yisy0 <yisy0703@naver.com>
2026-01-22 11:21:38 +0900

    Django ORM 1:N,1:1관계

--- source/12_django/myproject/blog/admin.py
+++ source/12_django/myproject/blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 admin.site.register(Post) # admin 페이지에 Post 테이블 액세스 가능
+admin.site.register(Comment)
