0e09e10c477902572da878fc0546d51ea0b33d46
yisy0 <yisy0703@naver.com>
2026-01-23 10:18:37 +0900

    Django ORM 템플릿

--- source/12_django/myproject/myproject/urls.py
+++ source/12_django/myproject/myproject/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.shortcuts import redirect
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index),
-    path("blog/", include("blog.urls"))
+    path("", lambda request : redirect("blog:list")),
+    path("blog/", include("blog.urls")),
 ]
