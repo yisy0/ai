341c792893d0eee70df3cc41d2c756deea9e9cb0
yisy0 <yisy0703@naver.com>
2026-01-23 10:03:18 +0900

    Django ORM 템플릿

--- source/12_django/myproject/accounts/models.py
+++ source/12_django/myproject/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  phone_number = models.CharField(verbose_name="전화", max_length=20)
+  address      = models.CharField(verbose_name="주소", max_length=100)
+  def __str__(self):
+    return "{}({}-{})".format(self.user.username, self.phone_number, self.address)
