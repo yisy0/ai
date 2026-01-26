6da1800a7f8150807479945b3063b4c847d00ba3
yisy0 <yisy0703@naver.com>
2026-01-26 11:04:44 +0900

    Django form객체

--- source/12_django/myproject/myproject/urls.py
+++ source/12_django/myproject/myproject/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", lambda request : redirect("blog:list")),
     path("blog/", include("blog.urls")),
+    path("book/", include("book.urls")),
 ]
