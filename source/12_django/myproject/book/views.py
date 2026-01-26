71bfdb843285102657df0e2565f19c4d63127da2
yisy0 <yisy0703@naver.com>
2026-01-26 12:04:43 +0900

    Django form객체

--- source/12_django/myproject/book/views.py
+++ source/12_django/myproject/book/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Book
 # 1. form없이 구현 2.form객체생성후(6장) 3.DjangoGenericView 이용 4.GenericView상속(7장)
@@ -13,7 +14,14 @@ def book_new(request):
     # 2) fail => 오류메세지와 함께 입력페이지로 이동 (form 객체 활용)
   if request.method == 'POST':
     # 파라미터 받아 ip까지 입력한 후 db에 저장
-    pass
+    title = request.POST.get('title')
+    author = request.POST.get('author')
+    publisher = request.POST.get('publisher')
+    sales = int(request.POST.get('sales'))
+    ip = request.META.get('REMOTE_ADDR', 'localhost')
+    book = Book(title=title, author=author, publisher=publisher, sales=sales, ip=ip)
+    book.save()
+    return red
   elif request.method == 'GET':
     return render(request, "book/book_form.html")
 
