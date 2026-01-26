22ca69146115055e3fe52138aa5babc0c0bf4d5a
yisy0 <yisy0703@naver.com>
2026-01-26 12:19:27 +0900

    Django form객체

--- source/12_django/myproject/book/forms.py
+++ source/12_django/myproject/book/forms.py
@@ -1,2 +1,19 @@
 from django import forms
 from .models import Book
+from django.core.validators import MinLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
+def min_length_2_validator(value):
+  if len(value) < 2:
+    raise forms.ValidationError("2글자 이상 입력하세요")
+  
+class BookForm(forms.Form):
+  title = forms.CharField(label="책이름")
+  author = forms.CharField(label="글쓴이", validators=[min_length_2_validator])
+  publisher = forms.CharField(label="출판사", required=False)
+  sales = forms.IntegerField(label="판매가", initial=1000,
+                            validators=[MinValueValidator(0), MaxValueValidator(100000)])
+  def save(self, commit=True):
+    book = Book(**self.cleaned_data) # cleaned_data입력데이터들을 검증 완료 데이터
+    if commit:
+      book.save()
+    return book
