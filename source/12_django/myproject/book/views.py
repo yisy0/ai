b67aed62537a3f16a06b8f5de4dfd885ffe6f5dd
yisy0 <yisy0703@naver.com>
2026-01-26 11:13:11 +0900

    Django form객체

--- source/12_django/myproject/book/views.py
+++ source/12_django/myproject/book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def book_list(request):
+  pass
+def book_new(request):
+  pass
+def book_edit(request, pk):
+  pass
+def book_delete(request, pk):
+  pass
