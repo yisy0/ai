98e27174ba12f568c414aeacfa99af2a60bb530c
yisy0 <yisy0703@naver.com>
2026-01-26 11:19:39 +0900

    Django form객체

--- source/12_django/myproject/book/views.py
+++ source/12_django/myproject/book/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book
-# 1. form없이 걍 2.form객체생성후
+# 1. form없이 구현 2.form객체생성후
 def book_list(request):
   return render(request, "book/book_list.html", {"book_list":Book.objects.all()})
 def book_new(request):
