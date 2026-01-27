d84643ae0773cfa27fad24abfb0633a9bfc1fc5c
yisy0 <yisy0703@naver.com>
2026-01-27 11:26:30 +0900

    Modelform view객체

--- source/12_django/myproject/book/views.py
+++ source/12_django/myproject/book/views.py
@@ -10,7 +10,7 @@ from .forms import BookModelForm
 #                 "object_list":Book.objects.all()})
 book_list = ListView.as_view(model=Book)
 
-def book_new(request):
+def book_new1(request):
   # GET : template 페이지(book/book_form.html)로 응답
   # POST : 파라미터변수받아 유효성 체크
     # 1) success => db에 insert -> book:list로
@@ -39,7 +39,21 @@ def book_new(request):
     form = BookModelForm()
   return render(request, "book/book_form.html", {'form':form})
 
-def book_edit(request, pk):
+book_new1 = CreateView.as_view(model = Book,
+                              fields = ['title', 'author', 'publisher', 'sales']
+                              ) # book.ip에는 null로 insert
+class BookCreateView(CreateView):
+  model = Book
+  fields = ['title', 'author', 'publisher', 'sales']
+  # template_name = "book/book_form.html"
+  def form_valid(self, form): # CreateView내의 함수를 오버라이드(재정의)
+    book = form.save(commit=False)
+    book.ip = self.request.META.get('REMOTE_ADDR', 'localhost')
+    book.save()
+    return redirect(book)
+book_new = BookCreateView.as_view()
+
+def book_edit1(request, pk):
   # GET : 수정 template페이지(book)만 응답
   # POST : 파라미터 받아 유효성 체크
     # 1)success : db에 update -> book:list
@@ -58,6 +72,9 @@ def book_edit(request, pk):
   elif request.method == 'GET':
     form = BookModelForm(instance=book)
   return render(request, "book/book_form.html", {"form":form})
+
+book_edit = UpdateView.as_view(model=Book,
+                              fields = ['title', 'author', 'publisher', 'sales'])
 
 def book_delete(request, pk):
   # book = get_object_or_404(Book, pk=pk)
