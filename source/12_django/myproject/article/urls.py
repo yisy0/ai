eb6bc8b1f40352da285782717c9a3642996ff31b
yisy0 <yisy0703@naver.com>
2026-01-28 10:22:49 +0900

    GenericView페이징처리

--- source/12_django/myproject/article/urls.py
+++ source/12_django/myproject/article/urls.py
@@ -3,12 +3,13 @@ from . import views
 # 기사목록 /article/          article:list
 # 기사추가 /article/new/      article:new
 # 기사상세 /article/1/detail  article:detail
-# 기사수정 /article/1/edit/   book:edit
-# 기사삭제 /article/1/delete/ book:delete
+# 기사수정 /article/1/edit/   article:edit
+# 기사삭제 /article/1/delete/ article:delete
 app_name = "article"
 urlpatterns = [
-  path("", views.book_list, name="list"),
-  path("new/", views.book_new, name="new"),
-  path("<int:pk>/edit/", views.book_edit, name="edit"),
-  path("<int:pk>/delete", views.book_delete, name="delete"),
+  path("", views.article_list, name="list"),
+  path("new/", views.article_new, name="new"),
+  path("<int:pk>/detail", views.artcle_detail, name="detail"),
+  path("<int:pk>/edit/", views.article_edit, name="edit"),
+  path("<int:pk>/delete", views.article_delete, name="delete"),
 ]
