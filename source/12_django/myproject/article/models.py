73c6f3f7392800718dbf7f9d3599ddeea38e2dad
yisy0 <yisy0703@naver.com>
2026-01-28 10:18:55 +0900

    GenericView페이징처리

--- source/12_django/myproject/article/models.py
+++ source/12_django/myproject/article/models.py
@@ -7,3 +7,12 @@ STATUS_CHOICES = (
   ('w', 'Withdrawn') # 철회
 )
 class Article(models.Model): # 테이블명 : article_article
+  title = models.CharField(verbose_name="제목", max_length=100)
+  body  = models.TextField(verbose_name="본문")
+  status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+  # photo 추가(파일첨부)
+  def __str__(self):
+    return f"{self.id}.{self.title}"
+  def get_absolute_url(self):
+    # return reverse("article:list") # create, update후 url
+    return reverse("article:detail", args=[self.id]) # create, update후 url : 해당 기사 상세보기
