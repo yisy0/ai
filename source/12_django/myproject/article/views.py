9a5f7a94f362f0f3f30ffeb518d2cd6b857f6737
yisy0 <yisy0703@naver.com>
2026-01-28 10:43:26 +0900

    GenericView페이징처리

--- source/12_django/myproject/article/views.py
+++ source/12_django/myproject/article/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .models import Article
 from django.core.paginator import Paginator
+from django.urls import reverse_lazy
 # /article : 1페이지   /article?page=3
 
 # article_list = ListView.as_view(model=Article, paginate_by=3) # 한페이지에 3행씩
