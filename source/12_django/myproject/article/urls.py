da0b42223dadb489407f9be6ecd8f23d7315ef8e
yisy0 <yisy0703@naver.com>
2026-01-29 10:06:22 +0900

    GenericView상속,파일업로드

--- source/12_django/myproject/article/urls.py
+++ source/12_django/myproject/article/urls.py
@@ -6,12 +6,12 @@ from . import views
 # 기사수정 /article/1/edit/   article:edit
 # 기사삭제 /article/1/delete/ article:delete
 # v2. 검색기능 O, 파일첨부
-from .views import ArticleListView, ArticleCreateView
+from .views import ArticleListView, ArticleCreateView, ArticleDetailView
 app_name = "article"
 urlpatterns = [
   path("", ArticleListView.as_view(), name="list"),
   path("new/", ArticleCreateView.as_view(), name="new"),
-  path("<int:pk>/detail", views.article_detail, name="detail"),
+  path("<int:pk>/detail", ArticleDetailView.as_view(), name="detail"),
   path("<int:pk>/edit/", views.article_edit, name="edit"),
   path("<int:pk>/delete", views.article_delete, name="delete")
 ]
