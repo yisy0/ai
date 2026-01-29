ec79f1940e872417c31c4ea9dfeaba32d1d2fba8
yisy0 <yisy0703@naver.com>
2026-01-29 10:50:35 +0900

    GenericView상속,파일업로드

--- source/12_django/myproject/article/urls.py
+++ source/12_django/myproject/article/urls.py
@@ -7,11 +7,12 @@ from . import views
 # 기사삭제 /article/1/delete/ article:delete
 # v2. 검색기능 O, 파일첨부
 from .views import ArticleListView, ArticleCreateView, ArticleDetailView
+from .views import ArticleUpdateView, ArticleDeleteView
 app_name = "article"
 urlpatterns = [
   path("", ArticleListView.as_view(), name="list"),
   path("new/", ArticleCreateView.as_view(), name="new"),
   path("<int:pk>/detail", ArticleDetailView.as_view(), name="detail"),
-  path("<int:pk>/edit/", views.article_edit, name="edit"),
-  path("<int:pk>/delete", views.article_delete, name="delete")
+  path("<int:pk>/edit/", ArticleUpdateView.as_view() , name="edit"),
+  path("<int:pk>/delete", ArticleDeleteView.as_view(), name="delete")
 ]
--- source/12_django/myproject/myproject/settings.py
+++ source/12_django/myproject/myproject/settings.py
@@ -133,14 +133,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+'''
+Django 프로젝트가 실행 -> 설정이 셋팅(settings.py)
+1. django.conf.global_settings.py 로드(모든 기본값)
+2. 사용자의 settings.py로드(프로젝트별 존재)
+'''
 # 개발 환경
-STATIC_URL = "static/" # 앱폴더 밑의 static
+STATIC_URL = "/static/" # 앱폴더 밑의 static
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myproject', 'static')
 ]
-# 배포 후 운영환경(2월)
+# 배포 후 운영환경(2월) : 모든 static을 STATIC_ROOT로 옮기기 - python manage.py collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# 업로드한 파일이 저장될 폴더와 액세스 url
+# /media/folder/a.png url로 접근 => _media/folder/a.png저장
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "_media")
+# myproject/urls.py에 MEDIA_URL와 ROOT연결
