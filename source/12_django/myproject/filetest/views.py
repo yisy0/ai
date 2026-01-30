d110490c2212028f660afcb6ad77443f7643b010
yisy0 <yisy0703@naver.com>
2026-01-30 10:45:09 +0900

    파일업로드

--- source/12_django/myproject/filetest/views.py
+++ source/12_django/myproject/filetest/views.py
@@ -5,10 +5,23 @@ import os
 # Create your views here.
 def upload_file(request):
   if request.method == 'POST':
-    # 파일첨부 처리
-    pass
-  else:
-    return render(request, "filetest/fileupload.html")
+    upload_file = request.FILES.get('파라미터이름')
+    MAX_FILE_SIZE = 10 * 1024 * 1024 # 최대 업로드 사이즈 : 10M
+    if upload_file and upload_file.size < MAX_FILE_SIZE:
+      # 10M미만 파일첨부함
+      upload_dir = os.path.join(settings.MEDIA_ROOT, "filetest")
+      os.makedirs(upload_dir, exist_ok=True) # 폴더가 없을 경우 폴더 생성
+      file_path = os.path.join(upload_dir, upload_file.name)
+      print(file_path, "에 저장함")
+      with open(file_path, "wb") as f:
+        for chunk in upload_file.chunks(): # 큰 파일 업로드시 조금씩 나눠서 저장
+          f.write(chunk)
+      return render(request, "filetest/result.html")
+    elif upload_file and upload_file.size >= MAX_FILE_SIZE:
+      # 10M이상 파일 첨부함
+      messages.error(request, f"파일사이즈({upload_file.size/(1024*1024)}M)")
+
+  return render(request, "filetest/fileupload.html")
 
 def predict(request):
   pass
