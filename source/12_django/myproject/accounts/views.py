265e51b80db5da11482056b7d35f0543a632484e
yisy0 <yisy0703@naver.com>
2026-02-02 11:38:01 +0900

    사용자인증

--- source/12_django/myproject/accounts/views.py
+++ source/12_django/myproject/accounts/views.py
@@ -11,7 +11,7 @@ def signup(request):
     if form.is_valid():
       profile = form.save()
       # 회원가입 후 로그인 페이지로 가기
-      return redirect(settings.LOGIN_URL)
+      return redirect(settings.LOGIN_URL) # 회원가입 성공 후 로그인 페이지로 
       # return redirect("/accounts/login")
       # return redirect("login")
   else:
@@ -23,6 +23,17 @@ from django.contrib.auth.views import LoginView, LogoutView
 login = LoginView.as_view(template_name="accounts/login_form.html") #로그인
 
 logout = LogoutView.as_view(next_page=settings.LOGIN_URL) # 로그아웃(로그아웃 후에는 로그인)
-
+# 회원정보 보기
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+@login_required
 def profile(request):
-  pass
+  profile, created = Profile.objects.get_or_create(
+    user = request.user
+  )
+  print("profile:", profile, "\t created:", created)
+  return render(request, "accounts/profile.html",
+                {"profile":profile, 
+                #"user":request.user,
+                "is_new_profile":created})
+  
