6a2907bca0a3fb19710c837b411bb24a9157c624
yisy0 <yisy0703@naver.com>
2026-02-02 11:06:29 +0900

    사용자인증

--- source/12_django/myproject/accounts/forms.py
+++ source/12_django/myproject/accounts/forms.py
@@ -6,7 +6,7 @@ class SignupForm(UserCreationForm):
   phone_number = forms.CharField(label="전화", max_length=20)
   address      = forms.CharField(label="주소", max_length=100)
   class Meta(UserCreationForm.Meta):
-    fields = UserCreationForm.Meta.fields + ['email']
+    fields = UserCreationForm.Meta.fields + ('email',)
   def save(self, commit=True):
     user = super().save() # auth_user테이블에 저장(username, password)
     profile = Profile(user=user,
--- source/12_django/myproject/accounts/views.py
+++ source/12_django/myproject/accounts/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 # 회원가입
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 from django.conf import global_settings as settings
+from django.shortcuts import redirect
 
 def signup(request):
   if request.method == 'POST':
-    pass # 회원가입 처리
+    form = SignupForm(request.POST)
+    # print(form.is_valid())
+    if form.is_valid():
+      profile = form.save()
+      # 회원가입 후 로그인 페이지로 가기
+      return redirect(settings.LOGIN_URL)
   else:
-    form = UserCreationForm()
+    #form = UserCreationForm()
+    form = SignupForm()
   return render(request, "accounts/signup_form.html", {"form":form})
 
 def login(request):
